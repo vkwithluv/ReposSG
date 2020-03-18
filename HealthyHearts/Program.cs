8ad23c8ea04f87e898ff97c203d158d31522745e
Veena White <50415483+vkwithluv@users.noreply.github.com>
2020-03-18 18:12:09 -0400

    Add files via upload

---
+++ HealthyHearts/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace HealthyHearts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            string InputUser;
+            int UserAge;
+            int Age = 0;
+            //int Max = (220-(Age));
+            //int TargetStart = (Max/2);
+            //double TargetEnd = (Max*(.85));
+
+
+            bool UserIsAlive = true;
+
+            do
+            {
+
+                Console.WriteLine("White is your age?");
+                InputUser = Console.ReadLine();
+                if (int.TryParse(InputUser, out UserAge))
+                {
+                    if (UserAge >= 1 && UserAge <= 110)
+                    {
+                        Age += UserAge;
+                        Console.Write("Your maximum heart rate should be "); Console.Write(220-UserAge); Console.Write(" beats per minute.\n");
+                        Console.Write("Your target HR Zone is "); Console.Write((220 - UserAge)/2); Console.Write(" - "); Console.Write((220 - UserAge)*(.85)); Console.Write(" beats per minute.\n");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("You are how old?! That can't be right.");
+                    }
+                    continue;
+                }
+            } while (UserIsAlive);
+        }
+    }
+}
